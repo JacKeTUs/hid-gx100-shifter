5aa184be48e19bdfbcd6b6dde5b60fdc00a3e31c
Oleg Makarenko <oleg@makarenk.ooo>
2026-01-20 04:59:37 +0200

    Add sysfs entries for calibration and for mode switching

--- hid-gx100-shifter.c
+++ hid-gx100-shifter.c
@@ -65,6 +65,146 @@ static const __u8 *shifter_report_fixup(struct hid_device *hid, __u8 *rdesc, uns
     return rdesc;
 }
 
+static bool sysfs_strtoint(int *out, const char* buf, size_t count)
+{
+    char tmp[32];
+    if (count >= sizeof(tmp) || !out || !buf)
+        return false;   
+    memcpy(tmp, buf, count);
+    tmp[count] = '\0';
+    return kstrtoint(tmp, 10, out) == 0;
+}
+
+static char current_mode = 'H';
+static short int calibration_h_started = 0;
+static short int calibration_seq_started = 0;
+
+
+static void set_calibration(int is_seq, int is_started)
+{
+    if (is_seq)
+        calibration_seq_started = is_started;
+    else
+        calibration_h_started = is_started;
+    // here we should send report to the device to start/end calibration
+}
+
+static void set_mode(char mode) {
+    current_mode = mode;
+    // here we should send report to the device to switch the mode
+}
+
+
+static ssize_t mode_store(struct device *dev, struct device_attribute *attr,
+                          const char *buf, size_t count)
+{
+    if (count < 1)
+        return -EINVAL;
+
+    if(buf[0] == 'H' || buf[0] == 'S')
+        set_mode(buf[0]);
+    else
+        return -EINVAL;
+
+    return count;
+}
+
+static ssize_t mode_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+    return sprintf(buf, "%c\n", current_mode);
+}
+
+static DEVICE_ATTR_RW(mode);
+
+static ssize_t calibration_h_store(struct device *dev, struct device_attribute *attr,
+                                   const char *buf, size_t count)
+{
+    int value;
+
+    if (!sysfs_strtoint(&value, buf, count))
+        return -EINVAL;
+
+    if (value == 0 || value == 1)
+        set_calibration(0, value);
+    else
+        return -EINVAL;
+
+    return count;
+}
+
+static ssize_t calibration_h_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+    return sprintf(buf, "%hd\n", calibration_h_started);
+}
+
+static DEVICE_ATTR_RW(calibration_h);
+
+static ssize_t calibration_seq_store(struct device *dev, struct device_attribute *attr,
+                                     const char *buf, size_t count)
+{
+    int value;
+
+    if (!sysfs_strtoint(&value, buf, count))
+        return -EINVAL;
+
+    if (value == 0 || value == 1)
+        set_calibration(1, value);
+    else
+        return -EINVAL;
+
+    return count;
+}
+
+static ssize_t calibration_seq_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+    return sprintf(buf, "%hd\n", calibration_seq_started);
+}
+
+static DEVICE_ATTR_RW(calibration_seq);
+
+static struct attribute *shifter_attrs[] = {
+    &dev_attr_mode.attr,
+    &dev_attr_calibration_h.attr,
+    &dev_attr_calibration_seq.attr,
+    NULL
+};
+
+static const struct attribute_group shifter_attr_group = {
+    .attrs = shifter_attrs
+};
+
+static int shifter_probe(struct hid_device *hdev, const struct hid_device_id *id)
+{
+    int ret;
+
+    ret = hid_parse(hdev);
+    if (ret) {
+        hid_err(hdev, "parse failed\n");
+        return ret;
+    }
+
+    ret = hid_hw_start(hdev, HID_CONNECT_DEFAULT);
+    if (ret) {
+        hid_err(hdev, "hw start failed\n");
+        return ret;
+    }
+
+    ret = sysfs_create_group(&hdev->dev.kobj, &shifter_attr_group);
+    if (ret) {
+        hid_err(hdev, "failed to create sysfs attributes\n");
+        hid_hw_stop(hdev);
+        return ret;
+    }
+
+    return 0;
+}
+
+static void shifter_remove(struct hid_device *hdev)
+{
+    sysfs_remove_group(&hdev->dev.kobj, &shifter_attr_group);
+    hid_hw_stop(hdev);
+}
+
 static const struct hid_device_id shifter_devices[] = {
     { HID_USB_DEVICE(USB_VENDOR_ID_GX, USB_PRODUCT_ID_GX100_HANDBRAKE) },
     { }
@@ -75,7 +215,9 @@ MODULE_DEVICE_TABLE(hid, shifter_devices);
 static struct hid_driver shifter_driver = {
     .name = "gx100-shifter",
     .id_table = shifter_devices,
-    .report_fixup = shifter_report_fixup
+    .report_fixup = shifter_report_fixup,
+    .probe = shifter_probe,
+    .remove = shifter_remove
 };
 module_hid_driver(shifter_driver);
 
